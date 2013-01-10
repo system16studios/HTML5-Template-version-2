3ddb1dd9eaa5f9bececaf67bac3de16098401c8c
system16studios <omicronten@gmail.com>
2013-01-10 14:45:47 -0500

    Tempate Version 2

---
+++ articles.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <title>HTML5 template no.2 (pure css3 driven effects only)</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="keywords" content="tabs, css3, transition, checked, pseudo-class, label, css-only, sibling combinator" />
+		<link rel="stylesheet" href="css/stylesheet.css" type="text/css" />
+	</head>
+	<body id="main" class="home">
+		<section id="top" class="body">
+		<nav>
+		<a href="home.html"><h1>Home</h1></a>
+		<a href="articles.php"><h1>Articles</h1></a>
+		<a href="media.html"><h1>Media</h1></a>
+		<a href="products.php"><h1>Products</h1></a>
+		<a href="contact.php"><h1>Contact</h1></a>
+		</nav>
+		<hr />
+		</section>
+		<section id="center" class="body">
+		<div id="demo">
+		
+		
+		</div>
+		<div id="demo2">
+		<h3> Popular Posts</h3>
+			<ul>
+				<li><a href="#">Link 1</a></li>
+				<li><a href="#">Link 2</a></li>
+				<li><a href="#">Link 3</a></li>
+				<li><a href="#">Link 4</a></li>
+			</ul>
+		<br />
+		<h3>Social Media</h3>
+			<ul>
+				<li><a href="#">Facebook</a></li>
+				<li><a href="#">Twitter</a></li>
+				<li><a href="#">Linked In</a></li>
+				<li><a href="#">Google+</a></li>
+			</ul>	
+		<h3> Relevant Links</h3>
+			<ul>
+				<li><a href="#">Link 1</a></li>
+				<li><a href="#">Link 2</a></li>
+				<li><a href="#">Link 3</a></li>
+				<li><a href="#">Link 4</a></li>
+			</ul>	
+		</div>	
+		</section>
+		<section id="bottom" class="body">
+		
+		
+		</section>
+		<footer id="credits" class="body">
+		<p> Copyright 2012</p>
+	</body>
+</html>	
